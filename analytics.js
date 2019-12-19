e80c95c7f28441475ea976582ba5c36b4e8a858a
cyneprepou4uk <cyneprepou4uk@gmail.com>
2019-12-20 00:02:03 +0300

    1

---
+++ analytics.js
@@ -0,0 +1,19 @@
+// yandex
+	(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+	m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+	(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+	
+	ym(56741584, "init", {
+		clickmap:true,
+		trackLinks:true,
+		accurateTrackBounce:true,
+		webvisor:true
+	});
+
+// google
+include('https://media.geeksforgeeks.org/wp-content/uploads/20190704153043/main.js'); 
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-154896299-1');
