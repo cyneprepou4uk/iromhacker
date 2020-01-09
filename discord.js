a575a197303c668d4369d27bffaf3426f2e6e6ff
cyneprepou4uk <cyneprepou4uk@gmail.com>
2020-01-09 19:50:45 +0300

    1

--- discord.js
+++ discord.js
@@ -1,13 +1,3 @@
-/*
-<script src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3" async defer>
-  new Crate({
-    server: '299881420891881473',
-    channel: '355719584830980096',
-    shard: 'https://disweb.dashflo.net'
-  })
-</script>
-*/
-
 function loadScript(src) {
     return new Promise(function (resolve, reject) {
         var s
@@ -32,3 +22,34 @@ loadScript('https://cdn.jsdelivr.net/npm/@widgetbot/crate@3')
 .catch(err => {
 	console.log(err)
 })
+
+
+
+/*
+<script src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3" async defer>
+  new Crate({
+    server: '299881420891881473',
+    channel: '355719584830980096',
+    shard: 'https://disweb.dashflo.net'
+  })
+</script>
+*/
+
+/* 
+<iframe src="https://titanembeds.com/embed/628272812439896075?defaultchannel=629795557761744896" height="600" width="100%" frameborder="0"></iframe>
+ */
+
+/* 
+<iframe src="https://disweb.dashflo.net/channels/628272812439896075/629795557761744896" height="600" width="100%"></iframe>
+ */
+
+/* 
+<widgetbot
+	server="628272812439896075"
+	channel="629795557761744896"
+	width="100%"
+	height="600"
+	shard="https://disweb.dashflo.net"
+></widgetbot>
+<script src="https://cdn.jsdelivr.net/npm/@widgetbot/html-embed"></script>
+ */
