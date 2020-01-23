a79c697a42a343fac62928928671a95717ba39fe
cyneprepou4uk <cyneprepou4uk@gmail.com>
2020-01-23 18:22:26 +0300

    Create smartsupp.js

---
+++ smartsupp.js
@@ -0,0 +1,8 @@
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '5fba978915b2e493f8c79e6eb8d54e1881fc22ea';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
