591c0056a7f6817c5b129fb953b5eb7f35468ba9
cyneprepou4uk <cyneprepou4uk@gmail.com>
2020-01-24 04:41:36 +0300

    Create livesupporti.js

---
+++ livesupporti.js
@@ -0,0 +1,5 @@
+    (function() {
+    var s = document.createElement('script');s.type='text/javascript';s.async=true;s.id='lsInitScript';
+    s.src='https://livesupporti.com/Scripts/clientAsync.js?acc=393ef334-9dad-4f34-88b4-5d35073280a8&skin=Air';
+    var scr=document.getElementsByTagName('script')[0];scr.parentNode.appendChild(s, scr);
+    })();
